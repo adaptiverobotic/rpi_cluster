f55e82cfc13d989db9b85ef0382b378070d7a692
Jabari Dash <jabari.dash@gmail.com>
2018-02-08 14:37:46 -0500

    ip list works, need to get self tho

--- ip/list.sh
+++ ip/list.sh
@@ -1,23 +1,22 @@
 # Get directory of this script
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Give arguments more
-# meaningful names
-ips="${DIR}/assets/ips"
+# Get paths to ips and filters files
+ips="${DIR}/../assets/ips"
 filters="${DIR}/assets/filters"
 
 echo "Creating list of ip addresses"
 
-# Delete old MAC address list
+# Delete old ip address list
 rm -f $ips
 
-# Create new empty file
+# Create new empty list file
 touch $ips
 
-# Read in device info in ; delimited list in format hostname;ip_address;mac_address
+# Read in device info as single string in ; delimited list in format hostname;ip_address;mac_address
 temp=$(arp -a | sed 's/^\([^ ][^ ]*\) (\([0-9][0-9.]*[0-9]\)) at \([a-fA-F0-9:]*\)[^a-fA-F0-9:].*$/\1;\2;\3/')
 
-# Convert to array
+# Convert list to array
 ip_macs=($temp)
 
 # Loop through array of hostname:ip:mac mappings
@@ -28,32 +27,27 @@ do
   # add to list of ips
   valid=false
 
+  # Separate ip and mac
+  while IFS=';' read -ra ADDR; do
+    ip=${ADDR[1]}
+    mac=${ADDR[2]}
+  done <<< "$i"
+
   # Check that MAC matches at least
   # one of the filters in the filter file
   while read line; do
-    echo $line
+    echo "$mac $line"
+
+    if [[ $mac == $(echo $line)* ]]; then
+      valid=true
+      break
+    fi
   done <$filters
 
   # If the current MAC matches,
   # write its corresponding ip
   # out to the ip_address file
-  if [[ $valid != true ]]; then
-     echo "valid mac: $i"
-    echo $i >> $ips
-   else
-     echo "invalid mac: $i"
+  if [[ $valid == true ]]; then
+    echo $ip >> $ips
    fi
 done
-
-# Create path to root ips file
-root_ips="${DIR}/../assets/ips"
-
-# Delete old file
-# if it exists
-rm -f $root_ips
-
-# Copy the ips file to root's
-# assets folder for other
-# scripts and services to access
-
-cp $ips $root_ips
