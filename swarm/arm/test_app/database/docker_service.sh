8d4bceb63ce605bc05f00c4d0088192bacbabb28
Jabari Dash <jabari.dash@gmail.com>
2018-02-07 19:52:21 -0500

    just added docker_service scrips because deploy annoying

---
+++ swarm/arm/test_app/database/docker_service.sh
@@ -0,0 +1,10 @@
+docker service create \
+--name database \
+--publish 5432:5432 \
+--replicas=1 \
+--constraint 'node.hostname == pi-0' \
+--mount type=volume,source=cluster_test_app_database,target=/var/lib/postgresql/data \
+--secret DEFAULT_PASSWORD \
+--env POSTGRES_PASSWORD_FILE=/run/secrets/DEFAULT_PASSWORD \
+--network cluster_test_app \
+jabaridash/cluster_test_app_database
