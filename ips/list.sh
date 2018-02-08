ac36ccedd95acd671f7da640ffe4e7591adee785
Jabari Dash <jabari.dash@gmail.com>
2018-02-08 10:06:56 -0500

    moved more folders, its bouta b lit

--- ips/list.sh
+++
@@ -1,3 +0,0 @@
-echo "Creating list of ips by MAC address"
-
-echo "Creating list of ips by hostname"
--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 # Build ip address list
-/bin/bash ips/list.sh
+/bin/bash ip/list.sh
 
 # Enable passwordless ssh
 /bin/bash ssh/install.sh
+
+# Change all the hostnames
+/bin/bash hostname/change.sh
+
+# Install dependencies
+/bin/bash dependencies/install.sh
 
 # Configure firewall
 /bin/bash ufw/install.sh
@@ -13,5 +19,5 @@
 # Initialize docker swarm
 /bin/bash docker/install.sh
 
-# Deploy test_app
+# Deploy test application
 /bin/bash docker/deploy/deploy.sh services
