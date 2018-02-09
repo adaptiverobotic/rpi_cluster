02b9e685895c0dd95b6d9d1c6d267881e3852512
Jabari Dash <jabari.dash@gmail.com>
2018-02-08 21:35:21 -0500

    dynamic naming of NAS working

--- samba/setup.sh
+++ samba/setup.sh
@@ -12,12 +12,23 @@ sudo mv $HOME/smb.conf $HOME/smb.conf.original
 # If something went wrong
 if [[ $? -ne 0 ]]; then
   echo "Could not backup /etc/samba/smb.conf"
+
+  # TODO - Perhaps put the old config back?
   exit 1
 fi
 
 # Recreate user and give it a password
 sudo pdbedit -x -u $user
 (echo $password; echo $password) | sudo smbpasswd -a $user
+
+hostname=$(cat /etc/hostname)
+drive_name="[home-$hostname]"
+
+echo "drive: $drive_name"
+
+# Modify config file so that we append the hostname
+# to the name of the network drive.
+sed -i "1s/.*/$drive_name/" smb.conf
 
 # Replace it with custom config file
 echo "Creating new config file"
