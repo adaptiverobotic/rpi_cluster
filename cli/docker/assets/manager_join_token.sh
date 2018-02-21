0509531734c09d1255bfd8daa3c46f36ed660ff4
Jabari Dash <jabari.dash@gmail.com>
2018-02-21 02:19:29 -0500

    cleaning docker dir

--- cli/docker/assets/manager_join_token.sh
+++
@@ -1,1 +0,0 @@
-    docker swarm join --token SWMTKN-1-5qf5vahv5p9dvuyvfnkm7tl2hqjfl84tekp836445nt9dl8gpy-9gkel2ewn7yebp5kdc5yf3c4h 192.168.2.77:2377
--- cli/docker/assets/worker_join_token.sh
+++
@@ -1,1 +0,0 @@
-    docker swarm join --token SWMTKN-1-5qf5vahv5p9dvuyvfnkm7tl2hqjfl84tekp836445nt9dl8gpy-65l923cm3f9c5qrf9oiiix5vp 192.168.2.77:2377
