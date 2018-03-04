917ad89593b487e9168ab5ed66a1d2d36ce89e9e
Jabari Dash <jabari.dash@gmail.com>
2018-03-03 23:22:25 -0500

    removed test_app

--- cli/deprecated/apps/test_app/server/docker_service.sh
+++
@@ -1,9 +0,0 @@
-docker service create \
---name server \
---publish 8085:8085 \
---replicas=1 \
---constraint 'node.role == manager' \
---secret DEFAULT_PASSWORD \
---env DB_PASSWORD_FILE=/run/secrets/DEFAULT_PASSWORD \
---network cluster_test_app \
-jabaridash/cluster_test_app_server
